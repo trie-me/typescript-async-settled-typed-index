240ac6ed0f085d7604b2a0fbe86928cb692a946c
trieme <89676758+trie-me@users.noreply.github.com>
2023-02-08 12:08:14 -0800

    Clarification and explanation

--- index.ts
+++ index.ts
@@ -1,49 +1,64 @@
-type ResolvedPromiseIndex<TState extends { [K: string]: Promise<any> }> = {
-    [key in keyof TState]?: Awaited<TState[key]>;
-  } & { errors: any[] };
-  
-  function isKeySetGuard(arg: any): arg is { keys: string[] } {
-    return arg?.keys && Array.isArray(arg.keys);
-  }
-  
-  async function settleAndResolve<
-    TState extends { [K: string]: Promise<any> },
-    TValue extends Awaited<TState[keyof TState]>
-  >(promiseIndex: TState): Promise<ResolvedPromiseIndex<TState>> {
-    const promises = Object.keys(promiseIndex).map((key: keyof TState) => {
-      return (promiseIndex[key] as Promise<TValue>).then(
-        (resolvedValue: TValue) => {
-          const partial = {} as Partial<{
-            [K in keyof TState]: Awaited<TState[K]>;
-          }>;
-          partial[key] = resolvedValue;
-          return partial;
-        }
-      );
-    });
-    const results = await Promise.allSettled(promises);
-    const result = results.reduce(
-      (acc, val) => {
-        if (val?.status === 'fulfilled') {
-          acc = { ...acc, ...val.value };
-        } else {
-          acc.errors.push(val.reason);
-        }
-        return acc;
-      },
-      { errors: [] } as ResolvedPromiseIndex<TState>
+type Merge<T> = { [key in keyof T]: T[key] }
+type ResolvedPromiseIndex<TState extends { [K: string]: Promise<any>}> = 
+{
+  [K in keyof TState]: Awaited<TState[K]>;
+}
+type ResolvedPromiseIndexWithError<TState extends { [K: string]: Promise<any> }> = ResolvedPromiseIndex<TState> & { errors: any[] };
+
+function isKeySetGuard(arg: any): arg is { keys: string[] } {
+  return arg?.keys && Array.isArray(arg.keys);
+}
+
+async function settleAndResolve<
+  TState extends { [K: string]: Promise<any> },
+  TValue extends Awaited<TState[keyof TState]>
+>(promiseIndex: TState): Promise<ResolvedPromiseIndexWithError<TState>> {
+  const promises = Object.keys(promiseIndex).map((key: keyof TState) => {
+    return (promiseIndex[key] as Promise<TValue>).then(
+      (resolvedValue: TValue) => {
+        /* Partial<ResolvedPromiseIndex<TState>> 
+           - is in the structural subset of ResolvedPromiseIndex<TState>   
+             - Which is in the structural subset of ResolvedPromiseIndexWithError<TState>
+        */
+        const partial = {} as Partial<ResolvedPromiseIndex<TState>>;
+        partial[key] = resolvedValue;
+        return partial;
+      }
     );
-    return result;
-  }
-  
-  const z = settleAndResolve({
-    alpha: Promise.resolve(1),
-    beta: Promise.resolve('b'),
-    delta: Promise.resolve({ waffles: 1231421 }),
-    gamma: Promise.reject(Error('boom')),
   });
-  z.then((v) => {
-    console.log(v.alpha, v.beta, v.delta, v.gamma, v.errors);
-  }).catch(v => console.error('Result: ', v.errors));
-  z.then(console.log);
-  
+  const results = await Promise.allSettled(promises);
+  const result = results.reduce(
+    (acc, val) => {
+      if (val?.status === 'fulfilled') {
+        acc = { ...acc, ...val.value };
+      } else {
+        acc.errors.push(val.reason);
+      }
+      return acc;
+    },
+    { errors: [] } as ResolvedPromiseIndexWithError<TState>
+  );
+  return result;
+}
+
+const z = settleAndResolve({
+  alpha: Promise.resolve(1),
+  beta: Promise.resolve('b'),
+  delta: Promise.resolve({ waffles: 1231421 }),
+  gamma: Promise.reject<string>(Error('boom')),
+});
+z.then((v) => {
+  console.log(v.alpha, v.beta, v.delta, v.gamma, v.errors);
+}).catch(v => console.error('Result: ', v.errors));
+z.then(console.log);
+
+
+// This section for type investigation
+type AsyncDefinition = {
+  alpha: Promise<number>,
+  beta: Promise<string>,
+  gamma: Promise<string>,
+  delta: Promise<{ waffles: number }>
+}
+type OutputObjectDefinition = ResolvedPromiseIndexWithError<AsyncDefinition>
+type Flattened = Merge<OutputObjectDefinition>;
